a9eafde2063d559d5836f0887326bce23c254413
ben <benrobertson9876@gmail.com>
2024-02-02 14:12:06 +1100

    remove results

--- plans/read_cert_requests.pp
+++ plans/read_cert_requests.pp
@@ -33,7 +33,7 @@ plan enterprise_toolbox::read_cert_requests (
 
   $task_results = run_task('enterprise_toolbox::read_csr', $pe_target_certname, { '_catch_errors' => true })
 
-  out::message("${$task_results[0].message} results")
+  # out::message("${$task_results[0].message}")
 
   $results = $task_results[0].message
   return($results)
