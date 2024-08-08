ec459057fd03f5aebcd5502b05e89e8f4e86a8d4
ben <benrobertson9876@gmail.com>
2024-08-09 09:44:50 +1000

    commit legacy facts

--- tasks/find_legacy_facts.rb
+++ tasks/find_legacy_facts.rb
@@ -2,11 +2,6 @@
 
 # facts def
 
-UNCONVERTIBLE_FACTS = ['memoryfree_mb', 'memorysize_mb', 'swapfree_mb',
-    'swapsize_mb', 'blockdevices', 'interfaces', 'zones',
-    'sshfp_dsa', 'sshfp_ecdsa', 'sshfp_ed25519',
-    'sshfp_rsa'].freeze
-
 REGEX_FACTS = [%r{^blockdevice_(?<devicename>.*)_(?<attribute>model|size|vendor)$},
     %r{^(?<attribute>ipaddress|ipaddress6|macaddress|mtu|netmask|netmask6|network|network6)_(?<interface>.*)$},
     %r{^processor(?<id>[0-9]+)$},
@@ -15,7 +10,11 @@ REGEX_FACTS = [%r{^blockdevice_(?<devicename>.*)_(?<attribute>model|size|vendor)
     %r{^ldom_(?<name>.*)$},
     %r{^zone_(?<name>.*)_(?<attribute>brand|iptype|name|uuid|id|path|status)$}].freeze
 
-EASY_FACTS = [
+LEGACY_FACTS = [
+    'memoryfree_mb', 'memorysize_mb', 'swapfree_mb',
+    'swapsize_mb', 'blockdevices', 'interfaces', 'zones',
+    'sshfp_dsa', 'sshfp_ecdsa', 'sshfp_ed25519',
+    'sshfp_rsa',
     'architecture',
     'augeasversion',
     'bios_release_date',
@@ -105,20 +104,37 @@ end
 files = []
 get_pp_files(files, "/etc/puppetlabs/code/environments/#{environment}/*", pattern)
 
-puts files
+# puts files
 
 def check_file(file)
     handle = File.open(file, 'r')
+    count = 1
     handle.each_line do | line |
+        # Check if line is commented
+        next if line.match?(/^#/)
         # check for easy facts.
-        EASY_FACTS.each do | easy |
-            if line.include? easy 
-                puts "File: #{file} contains leagcy fact #{easy}"
+        LEGACY_FACTS.each do | easy |
+            if line.include? "$facts['#{easy}']"
+                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
+            # elsif line.include? "$#{easy} "
+            elsif line.match?(/\$#{easy}[ %={]/)
+                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
+            # elsif line.include? "$::#{easy} "
+            elsif line.match?(/\$\:\:#{easy}[ %={]/)
+                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
             end
         end
+        REGEX_FACTS.each do | regex |
+            if line.match?(regex)
+                puts "File: #{file} contains leagcy fact #{regex} on line #{count}"
+            end
+        end
+        count += 1
     end
 end
 
 files.each do | file |
     check_file(file)
 end
+
+# could be a ' ' a % or an =
