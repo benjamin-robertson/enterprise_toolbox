8b5a7a04c89a7335ad1fd533c0fe587f5e27a006
ben <benrobertson9876@gmail.com>
2024-08-09 13:43:59 +1000

    commit changes

--- tasks/find_legacy_facts.rb
+++ tasks/find_legacy_facts.rb
@@ -1,6 +1,6 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
-# facts def
+# facts defs
 
 REGEX_FACTS = [%r{^blockdevice_(?<devicename>.*)_(?<attribute>model|size|vendor)$},
     %r{^(?<attribute>ipaddress|ipaddress6|macaddress|mtu|netmask|netmask6|network|network6)_(?<interface>.*)$},
@@ -13,81 +13,31 @@ REGEX_FACTS = [%r{^blockdevice_(?<devicename>.*)_(?<attribute>model|size|vendor)
 LEGACY_FACTS = [
     'memoryfree_mb', 'memorysize_mb', 'swapfree_mb',
     'swapsize_mb', 'blockdevices', 'interfaces', 'zones',
-    'sshfp_dsa', 'sshfp_ecdsa', 'sshfp_ed25519',
-    'sshfp_rsa',
-    'architecture',
-    'augeasversion',
-    'bios_release_date',
-    'bios_vendor',
-    'bios_version',
-    'boardassettag',
-    'boardmanufacturer',
-    'boardproductname',
-    'boardserialnumber',
-    'chassisassettag',
-    'chassistype',
-    'domain',
-    'fqdn',
-    'gid',
-    'hardwareisa',
-    'hardwaremodel',
-    'hostname',
-    'id',
-    'ipaddress',
-    'ipaddress6',
-    'lsbdistcodename',
-    'lsbdistdescription',
-    'lsbdistid',
-    'lsbdistrelease',
-    'lsbmajdistrelease',
-    'lsbminordistrelease',
-    'lsbrelease',
-    'macaddress',
-    'macosx_buildversion',
-    'macosx_productname',
-    'macosx_productversion',
-    'macosx_productversion_major',
-    'macosx_productversion_minor',
-    'manufacturer',
-    'memoryfree',
-    'memorysize',
-    'netmask',
-    'netmask6',
-    'network',
-    'network6',
-    'operatingsystem',
-    'operatingsystemmajrelease',
-    'operatingsystemrelease',
-    'osfamily',
-    'physicalprocessorcount',
-    'processorcount',
-    'productname',
-    'rubyplatform',
-    'rubysitedir',
-    'rubyversion',
-    'selinux',
-    'selinux_config_mode',
-    'selinux_config_policy',
-    'selinux_current_mode',
-    'selinux_enforced',
-    'selinux_policyversion',
-    'serialnumber',
-    'swapencrypted',
-    'swapfree',
-    'swapsize',
-    'system32',
-    'uptime',
-    'uptime_days',
-    'uptime_hours',
-    'uptime_seconds',
-    'uuid',
-    'xendomains',
-    'zonename'
+    'sshfp_dsa', 'sshfp_ecdsa', 'sshfp_ed25519', 'sshfp_rsa',
+    'architecture', 'augeasversion', 'bios_release_date', 'bios_vendor',
+    'bios_version', 'boardassettag', 'boardmanufacturer', 'boardproductname',
+    'boardserialnumber', 'chassisassettag', 'chassistype', 'domain',
+    'fqdn', 'gid', 'hardwareisa', 'hardwaremodel',
+    'hostname', 'id', 'ipaddress', 'ipaddress6',
+    'lsbdistcodename', 'lsbdistdescription', 'lsbdistid', 'lsbdistrelease',
+    'lsbmajdistrelease', 'lsbminordistrelease', 'lsbrelease', 'macaddress',
+    'macosx_buildversion', 'macosx_productname', 'macosx_productversion', 'macosx_productversion_major',
+    'macosx_productversion_minor', 'manufacturer', 'memoryfree', 'memorysize',
+    'netmask', 'netmask6', 'network', 'network6',
+    'operatingsystem', 'operatingsystemmajrelease', 'operatingsystemrelease', 'osfamily',
+    'physicalprocessorcount', 'processorcount', 'productname', 'rubyplatform',
+    'rubysitedir', 'rubyversion', 'selinux', 'selinux_config_mode',
+    'selinux_config_policy', 'selinux_current_mode', 'selinux_enforced', 'selinux_policyversion',
+    'serialnumber', 'swapencrypted', 'swapfree', 'swapsize',
+    'system32', 'uptime', 'uptime_days', 'uptime_hours',
+    'uptime_seconds', 'uuid', 'xendomains', 'zonename'
     ].freeze
 
 # Params here
 environment='development'
-pattern=[/\.pp$/,/\.epp$/]
+pattern=[/\.pp$/,/\.epp$/,/\.erb$/,/\.yaml$/]
+
+pattern.push(/\.rb/)
 
 def get_pp_files(files, folder, pattern)
     Dir.glob(folder) do | file |
@@ -107,29 +57,38 @@ get_pp_files(files, "/etc/puppetlabs/code/environments/#{environment}/*", patter
 # puts files
 
 def check_file(file)
-    handle = File.open(file, 'r')
-    count = 1
+    handle    = File.open(file, 'r')
+    count     = 0
     handle.each_line do | line |
+        count = count + 1
         # Check if line is commented
         next if line.match?(/^#/)
         # check for easy facts.
         LEGACY_FACTS.each do | easy |
-            if line.include? "$facts['#{easy}']"
-                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
-            # elsif line.include? "$#{easy} "
-            elsif line.match?(/\$#{easy}[ %={]/)
-                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
-            # elsif line.include? "$::#{easy} "
-            elsif line.match?(/\$\:\:#{easy}[ %={]/)
-                puts "File: #{file} contains leagcy fact #{easy} on line #{count}"
+            # check epp pp and erb
+            if file.match?(%r{\.pp$|\.epp$|\.erb$})
+                if line.match?(%r{[\$\@]facts\[\'#{easy}\'\]})
+                    puts "File: #{file} contains legacy fact #{easy} on line #{count}"
+                elsif line.match?(%r{[\$\@]#{easy}[ %={]})
+                    puts "File: #{file} contains legacy fact #{easy} on line #{count}"
+                elsif line.match?(/[\$\@]\:\:#{easy}[ %={]/)
+                    puts "File: #{file} contains legacy fact #{easy} on line #{count}"
+                end
+            elsif file.match?(/\.rb$/)
+                # check rb only
+                if line.match?(/Factor.value\(\'#{easy}\'\)/)
+                    puts "File: #{file} contains legacy fact #{easy} on line #{count}"
+                elsif line.match?(/confine #{easy}\:/)
+                    puts "File: #{file} contains legacy fact #{easy} on line #{count}"
+                end
+            elsif file.match?(/\.yaml$/)
             end
         end
         REGEX_FACTS.each do | regex |
             if line.match?(regex)
-                puts "File: #{file} contains leagcy fact #{regex} on line #{count}"
+                puts "File: #{file} contains legacy fact #{regex} on line #{count}"
             end
         end
-        count += 1
     end
 end
 
@@ -137,4 +96,8 @@ files.each do | file |
     check_file(file)
 end
 
-# could be a ' ' a % or an =
+# Facter.value('lsbdistid')
+# confine kernel:
+
+
+# hiera?
